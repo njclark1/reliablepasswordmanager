1c9a91543d38edfd82ea346dcec956a667defae2
njclark1 <77775946+njclark1@users.noreply.github.com>
2021-10-31 03:11:17 -0400

    new password form

--- lib/passwords.dart
+++ lib/passwords.dart
@@ -19,6 +19,9 @@ class Passwords extends StatefulWidget {
 
 class _PasswordsState extends State<Passwords> {
   @override
+
+  //variables
+
   Widget build(BuildContext context) {
     return Scaffold(
       //header bar properties
@@ -63,12 +66,48 @@ class _PasswordsState extends State<Passwords> {
   }
 }
 
+//new password form
 Widget createNewPassword(BuildContext context) {
+  String newServiceValue = "";
+  String newUsername = "";
+  String newPassword = "";
+  TextEditingController newServiceController = new TextEditingController();
+  TextEditingController newUsernameController = new TextEditingController();
+  TextEditingController newPasswordController = new TextEditingController();
   return new AlertDialog(
-    title: const Text('New Password'),
+    title: const Text('New service'),
     content: new Column(
       mainAxisSize: MainAxisSize.min,
-      children: <Widget>[Text("hi")],
+      children: <Widget>[
+        TextFormField(
+          controller: newServiceController,
+          onChanged: (String value) =>
+              newServiceValue = newServiceController.text,
+          obscureText: false,
+          decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              hintText: 'enter name of service',
+              labelText: 'Service'),
+        ),
+        TextFormField(
+          controller: newUsernameController,
+          onChanged: (String value) => newUsername = newUsernameController.text,
+          obscureText: false,
+          decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              hintText: 'enter username',
+              labelText: 'Username'),
+        ),
+        TextFormField(
+          controller: newPasswordController,
+          onChanged: (String value) => newPassword = newPasswordController.text,
+          obscureText: true,
+          decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              hintText: 'enter password',
+              labelText: 'Password'),
+        ),
+      ],
     ),
   );
 }
